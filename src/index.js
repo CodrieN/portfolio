e7bd371e69377582481670554b9901ebb976fa5c
CodrieN <adrien.marguin@gmail.com>
2023-08-07 09:09:51 +0200

    npm run build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Router from "./components/Router/Router";
@@ -10,10 +10,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div id="Flex">
-      <BrowserRouter>
+      <HashRouter basename="/">
         <Header />
         <Router />
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </div>
   </React.StrictMode>
